f41ccec04500924d2a6e6156ae3a238216a58904
Aditya Anand <adinbittu@gmail.com>
2025-07-08 12:19:41 +0530

    props

--- 1_Components/vite-project/src/main.jsx
+++ 1_Components/vite-project/src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function MyComponent(){
+  return <h2>Hello</h2>
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <MyComponent/>
+);
